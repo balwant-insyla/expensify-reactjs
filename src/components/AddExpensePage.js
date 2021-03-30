7dc6b57185a0c92e4ab2ecd0380e52b34c005750
Balwant Neeb <balwantneeb@Balwants-MacBook-Air.local>
2021-03-30 10:35:32 +0530

    Initial Commit

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AddExpensePage = () => (
+    <div>
+        This is Create page
+    </div>
+)
+export default AddExpensePage
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const EditExpensePage = (props) => (
+    <div>
+        Editing the expense of user with id of {props.match.params.id}
+    </div>
+)
+
+export default EditExpensePage
