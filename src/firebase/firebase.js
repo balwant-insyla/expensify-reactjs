2404fef69f05689c52fb6a0d78a1a4044b565c29
Balwant Neeb <balwantneeb@Balwants-MacBook-Air.local>
2021-04-09 10:03:44 +0530

    setting up firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAODuOpu9j_iRo8Sjv5yiHMAsyL7WT5kC8",
-    authDomain: "expensify-76e4f.firebaseapp.com",
-    projectId: "expensify-76e4f",
-    storageBucket: "expensify-76e4f.appspot.com",
-    messagingSenderId: "334318004554",
-    appId: "1:334318004554:web:77904f0a67bdef986039b3",
-    measurementId: "G-NG7ZZ6991L"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig); 
